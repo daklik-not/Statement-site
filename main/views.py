741a1d14da65685e557cd9054454e0d51c6a1c28
daklik-not <0987azaaz65@gmail.com>
2022-11-12 18:14:38 +0300

    Add static +templates.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+
+def hello_there(request):
+    zero=0
+    return render(request,'main/home.html',{"zero":zero})
